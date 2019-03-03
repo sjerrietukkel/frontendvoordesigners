210a4f615b8a9f473419341ab33dc3065ae0a9eb
sjerrietukkel <danielvdschuur@gmail.com>
2019-03-03 18:54:39 +0100

    added darkmode button, kinda works, no toggle

--- opdracht2/index.js
+++ opdracht2/index.js
@@ -1,1 +1,25 @@
-console.log('doe het ff')
+var slider = document.getElementById("slider")
+ res = document.getElementById("result")
+
+slider.addEventListener("input", function() {
+    res.innerHTML = "font-size " + slider.value
+}, false)
+
+function toggleNightMode() {
+    var color = document.getElementById('mydiv').style.color;
+      
+    var backgroundColor = document.getElementById('mydiv').style.backgroundColor;
+    
+      
+    if (color == "black" && backgroundColor == "white") {
+          document.getElementById('mydiv').style.color="white";
+          document.getElementById('mydiv').style.backgroundColor="black";
+          document.getElementById('mydiv').style.fontFamily="Open Sans";
+          
+      } else {
+          document.getElementById('mydiv').style.color="black";
+          document.getElementById('mydiv').style.backgroundColor="white";
+          document.getElementById('mydiv').style.fontFamily="Open Sans";
+      }
+  }
+  
