26b63bb70ad07d25966813b55b2ebf17209fc41f
sjerrietukkel <danielvdschuur@gmail.com>
2019-03-04 00:42:45 +0100

    grid added, need to fix sizeChange

--- opdracht2/index.js
+++ opdracht2/index.js
@@ -1,25 +1,47 @@
 var slider = document.getElementById("slider")
- res = document.getElementById("result")
+var res = document.getElementById("result")
+var settingsIcon = document.getElementById('settingsIcons')
+var main = document.getElementById('main')
+var p = document.querySelectorAll("p")
 
-slider.addEventListener("input", function() {
+p.style.color = "red"
+console.log(p)
+
+slider.addEventListener("input", sliderValue)
+
+function sliderValue() {
     res.innerHTML = "font-size " + slider.value
-}, false)
+}
+
+// p.forEach(function sizeChange(){
+//     p.style.fontSize = slider.value
+// })
 
 function toggleNightMode() {
-    var color = document.getElementById('mydiv').style.color;
+    var color = document.getElementById('darkmode').style.color;
       
-    var backgroundColor = document.getElementById('mydiv').style.backgroundColor;
+    var backgroundColor = document.getElementById('darkmode').style.backgroundColor;
     
       
     if (color == "black" && backgroundColor == "white") {
-          document.getElementById('mydiv').style.color="white";
-          document.getElementById('mydiv').style.backgroundColor="black";
-          document.getElementById('mydiv').style.fontFamily="Open Sans";
-          
+          document.getElementById('darkmode').style.color="white";
+          document.getElementById('darkmode').style.backgroundColor="black";      
       } else {
-          document.getElementById('mydiv').style.color="black";
-          document.getElementById('mydiv').style.backgroundColor="white";
-          document.getElementById('mydiv').style.fontFamily="Open Sans";
+          document.getElementById('darkmode').style.color="black";
+          document.getElementById('darkmode').style.backgroundColor="white";
       }
+  }
+
+  
+
+  settingsIcon.addEventListener('click', settingOn)
+  main.addEventListener('click', settingOff)
+
+  function settingOn() {
+    document.getElementById("settings").style.display = "block";
+  }
+  
+  function settingOff() {
+    document.getElementById("settings").style.display = "none";
   }
   
